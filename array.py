ec9f40f98e29af3daed3ecef5ec1245ed01fe3aa
Joanne Santhosh <joanne.santhosh@mths.ca>
2022-12-16 03:16:28 +0000

    update

--- array.py
+++ array.py
@@ -7,11 +7,10 @@
 import random
 
 
-def the_largest_number(list_of_numbers):
+def find_largest_number(list_of_numbers):
     largest_number = list_of_numbers[0]
 
-
-    for counter in range (0, len(list_of_numbers)):
+    for counter in range(0, len(list_of_numbers)):
         if list_of_numbers[counter] > largest_number:
             largest_number = list_of_numbers[counter]
 
@@ -19,18 +18,25 @@ def the_largest_number(list_of_numbers):
 
 
 def main():
-    # this function generates 10 random numbers and finds the largest one
+    # this function generates 10 random numbers
 
     random_numbers = []
 
-    # input
-    for counter in range(0, 10):
-        random_number = random.randint(0,100)
-        random_numbers.append(random_number)
-        print("{0} ".format(counter + 1, random_number))
-    largest_number = the_largest_number(random_number)
+    for loop_counter in range(0, 10):
+        single_random_number = random.randint(0, 100)
+        random_numbers.append(single_random_number)
+        print(
+            "The random number {0} is: {1}".format(
+                loop_counter + 1, random_numbers[loop_counter]
+            )
+        )
+
+    # calls function
+    largest_number = find_largest_number(random_numbers)
     print("")
-    print("The largest number is: {0} ".format(largest_number))
+    print("The largest number is {0}".format(largest_number))
+
+    print("\nDone.")
 
 
 if __name__ == "__main__":
